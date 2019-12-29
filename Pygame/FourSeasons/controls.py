ad320bd3efa48a57bfd673db91e81756ead5cefb
Alex E.T. Snyder <alexetsnyder@gmail.com>
2019-12-29 11:22:28 -0500

    Bug Fixes, and Formating
    You can choose the number of digits that the counter box starts with, and everything is more centered and sized correctly.

--- Pygame/FourSeasons/controls.py
+++ Pygame/FourSeasons/controls.py
@@ -369,9 +369,10 @@ class StopWatch (Control):
 		self.is_visible = True
 		self.time = 0
 		self.prv_time = 0
+		self.padding = 4
 		self.is_running = False
-		self.seperator_txt = go.RenderText(':')
-		self.font_info = go.FontInfo(font_size=30, font_color=digit_color)
+		self.font_info = go.FontInfo(font_size=20, font_color=digit_color)
+		self.seperator_txt = go.RenderText(':', self.font_info)
 		self.seconds_txt = go.RenderText(self.display_format(0), self.font_info)
 		self.minutes_txt = go.RenderText(self.display_format(0), self.font_info)
 		self.hours_txt = go.RenderText(self.display_format(0), self.font_info)
@@ -379,7 +380,7 @@ class StopWatch (Control):
 		super().__init__(left_top, default_size=self.get_clock_size())
 
 	def set_display(self):
-		hours, minutes, seconds = self.parse_time()
+		hours, minutes, seconds = self.get_time()
 		if hours >= 24:
 			self.time = 0
 			hours, minutes, seconds = (0, 0, 0)
@@ -397,28 +398,24 @@ class StopWatch (Control):
 	def set_position(self, left_top):
 		super().set_position(left_top)
 		self.box.set_position(self.left_top)
-		left = self.left + 5
+		left = self.left + self.padding * 2
 		self.hours_txt.set_position((left, self.y - self.hours_txt.h // 2))
-		left += self.hours_txt.w + self.seperator_txt.w + 4
+		left += self.hours_txt.w + self.seperator_txt.w + self.padding
 		self.minutes_txt.set_position((left, self.y - self.minutes_txt.h // 2))
-		left += self.minutes_txt.w + self.seperator_txt.w + 4
+		left += self.minutes_txt.w + self.seperator_txt.w + self.padding
 		self.seconds_txt.set_position((left, self.y - self.seconds_txt.h // 2))
 
 	def get_clock_size(self):
-		clock_width = 0
-		clock_width += self.hours_txt.w + 2
-		clock_width += self.seperator_txt.w + 2
-		clock_width += self.minutes_txt.w + 2
-		clock_width += self.seperator_txt.w + 2
-		clock_width += self.seconds_txt.w 
-		return (clock_width + 10, self.seconds_txt.h + 10)
+		clock_width =  self.padding + self.hours_txt.w + self.padding // 2
+		clock_width += self.seperator_txt.w + self.padding // 2
+		clock_width += self.minutes_txt.w + self.padding // 2
+		clock_width += self.seperator_txt.w + self.padding // 2
+		clock_width += self.seconds_txt.w + self.padding
+		return (clock_width, self.seconds_txt.h + self.padding)
 
-	def parse_time(self):
+	def get_time(self):
 		r_time = math.ceil(self.time)
 		return ((r_time//60//60), (r_time//60)%60, r_time%60)
-
-	def post_sidebar_refresh(self):
-		events.UserEvent(CustomEvent.REFRESH_SIDEBAR).post()
 
 	def set_visibility(self, is_visible):
 		self.is_visible = is_visible
@@ -442,14 +439,12 @@ class StopWatch (Control):
 	def tick(self):
 		if self.is_running:
 			current = time.time()
-			dt = current - self.prv_time 
-			self.time += dt 
+			self.time += current - self.prv_time 
 			self.prv_time = current
 
 	def update(self):
 		self.tick()
 		self.set_display()
-		self.set_size(self.get_clock_size())
 		self.set_position(self.origin)
 		self.seperator_txt.update()
 		self.seconds_txt.update()
@@ -458,14 +453,14 @@ class StopWatch (Control):
 		self.box.update()
 
 	def draw_clock(self, surface):
-		left = self.left + 5
+		left = self.left + self.padding + 2
 		self.hours_txt.draw(surface)
-		left += self.hours_txt.w
-		self.seperator_txt.draw_at(surface, (left + 1, self.top + self.h // 4 + 2))
-		left += self.seperator_txt.w + 7
+		left += self.hours_txt.w + self.padding
+		self.seperator_txt.draw_at(surface, (left, self.top + self.h // 4 + 2))
+		left += self.seperator_txt.w + self.padding // 2
 		self.minutes_txt.draw(surface)
-		left += self.minutes_txt.w
-		self.seperator_txt.draw_at(surface, (left - 1, self.top + self.h // 4 + 2))
+		left += self.minutes_txt.w + 2
+		self.seperator_txt.draw_at(surface, (left, self.top + self.h // 4 + 2))
 		self.seconds_txt.draw(surface)
 
 	def draw(self, surface):
@@ -474,18 +469,21 @@ class StopWatch (Control):
 		super().draw(surface)
 
 class CounterBox (Control):
-	def __init__(self, can_grow=False, limit=100, left_top=(0, 0), color=Color.BLACK, digit_color=Color.RED):
+	def __init__(self, digits, can_grow=False, limit=100, left_top=(0, 0), color=Color.BLACK, digit_color=Color.RED):
 		self.color = color 
 		self.digit_color = digit_color
 		self.limit = limit
 		self.can_grow = can_grow
-		self.div = 1000
+		self.digits = digits
+		self.post_update = False
+		self.div = 10 * int('{}{}'.format(1, ''.join(['0' for i in range(self.digits-1)])))
 		self.counter = 0
+		self.padding = 4
 		self.is_visible = True
 		self.box = go.Rect(left_top, (0, 0), color=color)
 		self.font_info = go.FontInfo(font_size=30, font_color=digit_color)
 		self.display_text = go.RenderText(self.get_display_str(), font_info=self.font_info)
-		super().__init__(left_top, default_size=self.display_text.size)
+		super().__init__(left_top, default_size=tuple(x + self.padding for x in self.display_text.size))
 
 	def set_size(self, size):
 		super().set_size(size)
@@ -493,11 +491,12 @@ class CounterBox (Control):
 
 	def set_position(self, left_top):
 		super().set_position(left_top)
-		self.box.set_position(self.left_top)
+		self.box.set_position((self.left, self.top))
 		self.display_text.center_on(self.center)
 
 	def get_display_str(self):
-		return '{:03}'.format(self.counter)
+		format_str = '{:0' + str(self.digits) + '}'
+		return format_str.format(self.counter)
 
 	def post_sidebar_refresh(self):
 		events.UserEvent(CustomEvent.REFRESH_SIDEBAR).post()
@@ -515,8 +514,8 @@ class CounterBox (Control):
 
 	def update_counter(self):
 		if self.can_grow and self.counter / self.div >= 1:
-			print('sidebar refresh {:03}'.format(self.counter))
 			self.div *= 10
+			self.post_update = True
 			self.post_sidebar_refresh()
 		elif not self.can_grow and self.counter >= self.limit:
 			self.counter = 0
@@ -524,8 +523,10 @@ class CounterBox (Control):
 	def update(self):
 		self.update_counter()
 		self.display_text.set_text(self.get_display_str())
-		self.set_size(self.display_text.size)
-		self.set_position(self.origin)
+		if self.post_update:
+			self.set_size(tuple(x + self.padding for x in self.display_text.size))
+			self.set_position(self.origin)
+			self.post_update = False
 		self.display_text.update()
 		self.box.update()
 
@@ -546,8 +547,8 @@ if __name__=='__main__':
 	bottom_btn = Button('Bottom')
 	disable_btn = Button('Disable')
 	check_box = CheckBox('Checked', on_checked= lambda event : print('(Un)Checked!'))
-	counter_box = CounterBox(can_grow=True)
-	increment_btn = Button('Increment', lambda event : counter_box.increment(10))
+	counter_box = CounterBox(4, can_grow=True)
+	increment_btn = Button('Increment', lambda event : counter_box.increment(1000))
 	stop_watch = StopWatch()
 	start_btn = Button('Start', lambda event : stop_watch.start())
 	stop_btn = Button('Stop', lambda event : stop_watch.stop())
@@ -559,13 +560,13 @@ if __name__=='__main__':
 	controls.append(top_btn)
 	controls.append(bottom_btn)
 	controls.append(disable_btn)
-	controls.append(check_box)
 	controls.append(counter_box)
 	controls.append(increment_btn)
 	controls.append(stop_watch)
 	controls.append(start_btn)
 	controls.append(stop_btn)
 	controls.append(reset_btn)
+	controls.append(check_box)
 	side_bar = SideBar((600, 400), WindowSide.LEFT, controls=controls)
 
 	def on_left_click(event):
@@ -585,6 +586,7 @@ if __name__=='__main__':
 		left_btn.set_enabled(controls_enabled)
 		right_btn.set_enabled(controls_enabled)
 		top_btn.set_enabled(controls_enabled)
+		bottom_btn.set_enabled(controls_enabled)
 		check_box.set_enabled(controls_enabled)
 		increment_btn.set_enabled(controls_enabled)
 		start_btn.set_enabled(controls_enabled)
